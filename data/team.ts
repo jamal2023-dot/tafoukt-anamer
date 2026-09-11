552be9ebdf036ce1766414185b3fb259bd654b3b
Jamal Karami <jamal2023-dot@users.noreply.github.com>
2026-09-11 09:16:38 +0100

    fix: correct French board member names

--- data/team.ts
+++ data/team.ts
@@ -12,21 +12,21 @@ export type TeamMember = {
 export const team: TeamMember[] = [
   {
     id: 'president-ibrahim-ouhmid',
-    name: { fr: 'Ibrahim Ouhmid', ar: 'احميد ابراهيم', en: 'Ibrahim Ouhmid' },
+    name: { fr: 'Brahim OUHMID', ar: 'احميد ابراهيم', en: 'Ibrahim Ouhmid' },
     role: { fr: 'Président', ar: 'الرئيس', en: 'President' },
     image: '/images/team/ibrahim-ouhmid.png',
     imagePosition: 'center 28%',
   },
   {
     id: 'vice-president-hassan-karami',
-    name: { fr: 'Hassan Karami', ar: 'كرامي حسن', en: 'Hassan Karami' },
+    name: { fr: 'Hassan KARAMI', ar: 'كرامي حسن', en: 'Hassan Karami' },
     role: { fr: 'Vice-président', ar: 'نائب الرئيس', en: 'Vice-president' },
     image: '/images/team/hassan-karami.png',
     imagePosition: 'center 32%',
   },
   {
     id: 'treasurer-rachid-ankmar',
-    name: { fr: 'Rachid Ankmar', ar: 'انكمار رشيد', en: 'Rachid Ankmar' },
+    name: { fr: 'Rachid ANOUGMAR', ar: 'انكمار رشيد', en: 'Rachid Ankmar' },
     role: { fr: 'Trésorier', ar: 'الامين', en: 'Treasurer' },
     image: '/images/team/rachid-ankmar.png',
     imagePosition: 'center top',
@@ -35,7 +35,7 @@ export const team: TeamMember[] = [
   {
     id: 'assistant-treasurer-abdellah-karami',
     name: {
-      fr: 'Abdellah Karami',
+      fr: 'Abdellah KARAMI',
       ar: 'كرامي عبد الله',
       en: 'Abdellah Karami',
     },
@@ -50,7 +50,7 @@ export const team: TeamMember[] = [
   {
     id: 'secretary-el-houssine-lotfi',
     name: {
-      fr: 'El Houssine Lotfi',
+      fr: 'Lahoucine LOTFI',
       ar: 'لطفي الحسين',
       en: 'El Houssine Lotfi',
     },
@@ -60,7 +60,7 @@ export const team: TeamMember[] = [
   },
   {
     id: 'assistant-secretary-jamal-karami',
-    name: { fr: 'Jamal Karami', ar: 'كرامي جمال', en: 'Jamal Karami' },
+    name: { fr: 'Jamal KARAMI', ar: 'كرامي جمال', en: 'Jamal Karami' },
     role: {
       fr: 'Secrétaire adjoint',
       ar: 'نائب الكاتب',
@@ -71,7 +71,7 @@ export const team: TeamMember[] = [
   {
     id: 'advisor-abdellah-karami',
     name: {
-      fr: 'Abdellah Karami',
+      fr: 'Abdellah KARAMI',
       ar: 'كرامي عبد الله',
       en: 'Abdellah Karami',
     },
